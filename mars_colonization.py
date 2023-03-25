a566d1623c7d6e3a01de30fd02c550a8ef2c4f6b
Saricie <sariciegh@gmail.com>
2023-03-25 21:22:12 +0500

    image

--- mars_colonization.py
+++ mars_colonization.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, url_for
 
 
 app = Flask(__name__)
@@ -20,6 +20,13 @@ def promotion():
           "Мы сделаем обитаемыми безжизненные пока планеты.", "И начнем с Марса!",
           "Присоединяйся!"]
     return "</br>".join(pr)
+
+
+@app.route("/image_mars")
+def image_mars():
+    image = f'''<img src="{url_for('static', filename='img/mars.jpg')}" 
+           alt="здесь должна была быть картинка, но не нашлась">'''
+    return f"<h1>Жди нас, Марс!</h1></br>{image}</br>Вот она какая, красная планета"
 
 
 if __name__ == '__main__':
