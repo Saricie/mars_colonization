e186518eb1545bb572b848ae7517ba02ba874384
Saricie <sariciegh@gmail.com>
2023-03-26 22:21:38 +0500

    choice planet

--- mars_colonization.py
+++ mars_colonization.py
@@ -195,5 +195,37 @@ def astronaut_form():
                             </html>'''
 
 
+@app.route("/choice/<planet_name>")
+def choice(planet_name):
+    return f'''<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                    <link rel="stylesheet"
+                   href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+                   integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+                   crossorigin="anonymous">
+                    <title>{planet_name}</title>
+                  </head>
+                  <body>
+                    <h1>Выбор: {planet_name}</h1>
+                    <h2>Она находится в пределах нашей досягаемости</h2>
+                    <div class="alert alert-primary" role="alert">
+                      Незаселена, пока что ;)
+                    </div>
+                    <div class="alert alert-primary" role="alert">
+                      Лучший выбор
+                    </div>
+                    <div class="alert alert-primary" role="alert">
+                      Определённо точно
+                    </div>
+                    <div class="alert alert-primary" role="alert">
+                      Комментарий
+                    </div>
+                  </body>
+                </html>'''
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
