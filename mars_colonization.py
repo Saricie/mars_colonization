5699cf65d05ab56c1adb3683ba3d06627e86d286
Saricie <sariciegh@gmail.com>
2023-03-26 21:44:37 +0500

    promotion image 2

--- mars_colonization.py
+++ mars_colonization.py
@@ -51,8 +51,12 @@ def promotion_image():
                   <head>
                     <meta charset="utf-8">
                     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-                    <link rel="stylesheet" 
-                    href=style.css />
+                    <link rel="stylesheet"
+                        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+                        integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+                        crossorigin="anonymous">
+                    <link rel="stylesheet" type="text/css" href="{url_for('static', 
+                                                                            filename='css/style.css')}" />
                     <title>Колонизация</title>
                   </head>
                   <body>
@@ -76,6 +80,80 @@ def promotion_image():
                     </div>
                   </body>
                 </html>'''
+
+
+@app.route("/astronaut_selection")
+def astronaut_form():
+        return f'''<!doctype html>
+                            <html lang="en">
+                              <head>
+                                <meta charset="utf-8">
+                                <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                                <link rel="stylesheet"
+                                href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+                                integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+                                crossorigin="anonymous">
+                                <link rel="stylesheet" type="text/css" href="{url_for('static', 
+                                                                            filename='css/astronaut_form.css')}" />
+                                <title>Отбор астронавтов</title>
+                              </head>
+                              <body>
+                                <h1>Анкета претендента</h1>
+                                <h2>на участие в миссии</h2>
+                                <div>
+                                    <form class="login_form" method="post">
+                                        <input type="surname" class="form-control" id="surname" 
+                                        placeholder="Ваша фамилия" name="surname">
+                                        <input type="name" class="form-control" id="name" 
+                                        placeholder="Ваше имя" name="name"> <br>
+                                        <input type="email" class="form-control" id="email" 
+                                        placeholder="Ваш email" name="email"> <br>
+                                        <div class="form-group">
+                                            <label for="educationSelect">Ваше образование</label>
+                                            <select class="form-control" id="educationSelect" name="education">
+                                              <option>Начальное</option>
+                                              <option>Основное общее</option>
+                                              <option>Среднее общее</option>
+                                              <option>Среднее профессиональное</option>
+                                              <option>Высшее</option>
+                                            </select>
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="about">Немного о себе</label>
+                                            <textarea class="form-control" id="about" rows="3" name="about"></textarea>
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="photo">Приложите фотографию</label>
+                                            <input type="file" class="form-control-file" id="photo" name="file">
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="form-check">Укажите пол</label>
+                                            <div class="form-check">
+                                              <input class="form-check-input" type="radio" name="sex" 
+                                              id="male" value="male" checked>
+                                              <label class="form-check-label" for="male">
+                                                Мужской
+                                              </label>
+                                            </div>
+                                            <div class="form-check">
+                                              <input class="form-check-input" type="radio" name="sex" 
+                                              id="female" value="female">
+                                              <label class="form-check-label" for="female">
+                                                Женский
+                                              </label>
+                                            </div>
+                                        </div>
+                                        <div class="form-group form-check">
+                                            <input type="checkbox" class="form-check-input" 
+                                            id="acceptRules" name="accept">
+                                            <label class="form-check-label" 
+                                            for="acceptRules">Готов быть добровольцем</label>
+                                        </div>
+                                        <button type="submit" class="btn btn-primary">Записаться</button>
+                                    </form>
+                                </div>
+                              </body>
+                            </html>'''
 
 
 if __name__ == '__main__':
