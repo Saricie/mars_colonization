23a54db44a1433401f8f39f4795fe2f74925af5e
Saricie <sariciegh@gmail.com>
2023-04-03 14:20:25 +0500

    Тренировки

--- mars_colonization.py
+++ mars_colonization.py
@@ -7,7 +7,20 @@ app = Flask(__name__)
 @app.route('/<title>')
 @app.route('/index/<title>')
 def main(title):
-    return render_template("base.html", title=title)
+    param = dict()
+    param["title"] = title
+    return render_template("base.html", **param)
+
+
+@app.route('/training/<prof>')
+def training(prof):
+    param = dict()
+    param["title"] = 'Симуляторы'
+    if 'инженер' in prof.lower() or 'строитель' in prof.lower():
+        param["prof"] = 'Инженерные симуляторы'
+    else:
+        param["prof"] = 'Научные симуляторы'
+    return render_template("training.html", **param)
 
 
 if __name__ == '__main__':
