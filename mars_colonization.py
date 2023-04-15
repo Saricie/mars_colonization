67d86c3b79ec7b28bd396803413a228ae0be41ed
Saricie <sariciegh@gmail.com>
2023-04-15 18:12:00 +0500

    Ответ

--- mars_colonization.py
+++ mars_colonization.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
 
 
 app = Flask(__name__)
+
+astro_form_d = dict()
 
 
 @app.route('/<title>')
@@ -32,6 +34,29 @@ def list_prof(profs_list):
     elif profs_list == "ul":
         param["list"] = "ul"
     return render_template("list_prof.html", **param)
+
+
+@app.route("/astronaut_selection", methods=['POST', 'GET'])
+def astronaut_form():
+    if request.method == 'GET':
+        return render_template("astronaut_form.html")
+    elif request.method == 'POST':
+        astro_form_d["surname"] = request.form.get("surname")
+        astro_form_d["name"] = request.form.get("name")
+        astro_form_d["email"] = request.form.get("email")
+        astro_form_d["education"] = request.form.get("education")
+        astro_form_d["job"] = request.form.get("job")
+        astro_form_d["sex"] = request.form.get("sex")
+        astro_form_d["about"] = request.form.get("about")
+        astro_form_d["accept"] = request.form.get("accept")
+        return redirect('/auto_answer')
+
+
+@app.route("/answer")
+@app.route("/auto_answer")
+def answer():
+    astro_form_d["title"] = "Ответ"
+    return render_template("auto_answer.html", **astro_form_d)
 
 
 if __name__ == '__main__':
